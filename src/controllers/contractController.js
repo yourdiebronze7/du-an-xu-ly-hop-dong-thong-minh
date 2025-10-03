f476adbd0db8e094f1730ecf72fefb16ad060a1a
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-10-03 16:20:10 +0000

    feat: initial commit for src/controllers/contractController.js

---
+++ src/controllers/contractController.js
@@ -0,0 +1,9 @@
+exports.createContract = (req, res) => {
+  // Logic tạo hợp đồng thông minh
+  res.send('Hợp đồng thông minh đã được tạo!');
+};
+
+exports.getContracts = (req, res) => {
+  // Logic lấy danh sách hợp đồng
+  res.send('Danh sách hợp đồng');
+};
